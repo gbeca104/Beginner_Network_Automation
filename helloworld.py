aa9b3988bd22893e501e72c8ed6de378294b1846
Gbeca <gbeca104@gmail.com>
2026-02-17 21:17:12 +0000

    First SSH->Router interaction w/password protection

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,38 @@
 # APENAS UM EXEMPLO DE CÓDIGO PARA TESTAR O AMBIENTE DE DESENVOLVIMENTO
+import os
+from dotenv import load_dotenv
+from netmiko import ConnectHandler
 
-print("Hello, World!")
+#loads the environment variables from the .env file
+load_dotenv() 
+
+#Retrieve the password from the environment variable. 
+secret_password = os.getenv("Costa_Lab_password")
+
+
+router_cisco = {
+    'device_type': 'cisco_ios',
+    'host': '192.168.1.254',
+    'username': 'admin',
+    'password': secret_password,
+}
+
+print("Connecting to the router...")
+
+# Establishing a connection to the router using ConnectHandler.
+try:
+    net_connect = ConnectHandler(**router_cisco)
+    
+    # 3. Enviar um comando e guardar a resposta
+    output = net_connect.send_command("show vtp status")
+    
+    print("-" * 30)
+    print("Interfaces state:")
+    print(output)
+    print("-" * 30)
+
+    # Closing the connection.
+    net_connect.disconnect()
+
+except Exception as e:
+    print(f"Error Connecting to Router: {e}")
